5607419a875ac92692d3441021618ea82455e935
alexcyterski <alexcyterski@gmail.com>
2025-03-02 22:05:56 -0500

    fix some bugs

--- visualizer_project/settings.py
+++ visualizer_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=f+wgdvp9^g-zq$sfh_!gi_mp3w2kigrzo*qy6qw*)(l1+pm3$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['192.168.0.108', 'localhost', '127.0.0.1']
 
 
 # Application definition
